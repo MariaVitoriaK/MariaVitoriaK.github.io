902ed4c6528e55962fdf7ce331a70444fe3d3f5b
Vi <197960@upf.br>
2024-06-10 20:38:39 -0300

    blog

---
+++ javascript/java.js
@@ -0,0 +1,23 @@
+function calcular(){
+    for (let i=0; i<=99; i++){
+        console.log
+    }
+}
+
+function parOuImpar(valor) {
+
+    let resultado = "";
+
+    if(valor % 2 == 0){
+        resultado = "par";
+    }else {
+        resultado = "impar"
+    }
+    console.log("O valor" + valor + "é" + resultado)
+}
+
+function ex() {
+    for(let x = 10; x > 0; x--){
+        console.log(x)
+    }
+}
